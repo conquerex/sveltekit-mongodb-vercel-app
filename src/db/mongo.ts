a7130760af54f280139f855a8f2db07c05888469
Jongkook <barley.son@ip-172-30-1-75.ap-northeast-2.compute.internal>
2022-10-25 16:00:51 +0900

    4. DB 연동

---
+++ src/db/mongo.ts
@@ -0,0 +1,12 @@
+import { MongoClient } from 'mongodb';
+import { MONGO_URL } from '$env/static/private';
+
+console.log('url >>> ' + MONGO_URL);
+const client = new MongoClient(MONGO_URL);
+
+export function start_mongo(): Promise<MongoClient> {
+	console.log('Starting mongo...');
+	return client.connect();
+}
+
+export default client.db('mydb');
